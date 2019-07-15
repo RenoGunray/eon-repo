9eaaf97b3caceacd5f52506b39bf80c9f73845e5
RenoGunray <52301114+RenoGunray@users.noreply.github.com>
2019-07-15 22:45:51 +0200

    second commit

--- nodeProject.js
+++ nodeProject.js
@@ -1,14 +1,15 @@
 
 var admin = require("firebase");
 
-var serviceAccount = require("./node-project-246513-4399114772d4.json");
+//var serviceAccount = require("./node-project-246513-4399114772d4.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  serviceAccount: "./node-project-246513-4399114772d4.json",
+  //credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://node-project-246513.firebaseio.com"
 });
 
-var ref = firebase.database().ref("somedata");
+/*var ref = firebase.database().ref("somedata");
 var newNode = ref.child('messages');
 
 newNode.push({
@@ -16,7 +17,26 @@ newNode.push({
   admin: true,
   count: 1,
   text: 'my oh my'
-});
+});*/
+
+var db = firebase.database();
+var ref = db.ref("./humans");
+
+ref.set([
+  {
+    id: "0",
+    name: "theodore",
+    email: "theo@gmail.com",
+    hobbis: "listen to Rock and Roll"
+  },
+
+  {
+    id: "1",
+    name: "nkhwamba",
+    email: "nkm@yahoo.com",
+    hobbis: "watch anime"
+  }
+]);
 
 
 
