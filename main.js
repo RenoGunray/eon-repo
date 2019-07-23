1e0072a350b83855ab8c6e150d14ff67a5ccbdf4
RenoGunray <52301114+RenoGunray@users.noreply.github.com>
2019-07-23 17:30:33 +0200

    adding members to database

--- main.js
+++ main.js
@@ -6,9 +6,46 @@ firebase.initializeApp({
   databaseURL: "https://node-project-246513.firebaseio.com"
 });
 
+//inserting data for the member form
+const addMemberBtn = document.getElementById('add-member-btn');
+const insertName = document.getElementById('name').value;
+const insertEmail = document.getElementById('email').value;
+const insertPhone = document.getElementById('phone').value;
+
+
 
 var db = firebase.database();
 var ref = db.ref("/humans");
+
+addMemberBtn.addEventListener('click', function(){
+  ref = db.ref('/humans' + insertName);
+  ref.push({
+    email: insertEmail,
+    phone: insertPhone 
+  });
+});
+
+//inserting data for corporate body form
+
+const addCorporateBtn = document.getElementById('add-corporate-btn');
+const insertCorpName = document.getElementById('corp-name').value;
+const insertCorpEmail = document.getElementById('corp-email').value;
+const insertCorpContact = document.getElementById('corp-contact').value;
+
+
+
+var db = firebase.database();
+var ref = db.ref("/humans");
+
+addCorporateBtn.addEventListener('click', function(){
+  ref = db.ref('/humans' + insertCorpName);
+  ref.push({
+    email: insertCorpEmail,
+    phone: insertCorpContact
+  });
+});
+
+//
 
 ref.set([
   {
